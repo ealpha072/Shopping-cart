7568c75621165c6738d0c45ceca343ef785158d7
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-10-30 15:31:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,41 @@
-let images = [{
-	'url':'images/1 (1).jpg',
-	'price': '10,000'
-},{
-	'url':'images/1 (2).jpg',
-	'price':'15,500'
+let images = 
+    [{
+	   'url':'images/1 (1).jpg',
+	   'price': '10,000'
+    },{
+    	'url':'images/1 (2).jpg',
+    	'price':'15,500'
 
-},{
-	'url':'images/1.jpg',
-	'price':'14,200'
-},{
-	'url':'images/1 (4).jpg',
-	'price':'17,900'
-},{
-	'url':'images/1 (5).jpg',
-	'price':'13,275'
-},{
-	'url':'images/1 (6).jpg',
-	'price':'16,300'
-},{
-	'url':'images/1 (7).jpg',
-	'price':'19,400'
-},{
-	'url':'images/1 (8).jpg',
-	'price':'20,500'
-},{
-	'url':'images/1 (9).jpg',
-	'price':'21,000'
-},{
-	'url':'images/1 (10).jpg',
-	'price':'22,400'
-},{
-	'url':'images/1 (11).jpg',
-	'price':'23,100'
-},{
-	'url':'images/1 (12).jpg',
-	'price':'12,800'
+    },{
+    	'url':'images/1.jpg',
+    	'price':'14,200'
+    },{
+    	'url':'images/1 (4).jpg',
+    	'price':'17,900'
+    },{
+    	'url':'images/1 (5).jpg',
+    	'price':'13,275'
+    },{
+    	'url':'images/1 (6).jpg',
+    	'price':'16,300'
+    },{
+    	'url':'images/1 (7).jpg',
+    	'price':'19,400'
+    },{
+    	'url':'images/1 (8).jpg',
+    	'price':'20,500'
+    },{
+    	'url':'images/1 (9).jpg',
+    	'price':'21,000'
+    },{
+    	'url':'images/1 (10).jpg',
+    	'price':'22,400'
+    },{
+    	'url':'images/1 (11).jpg',
+    	'price':'23,100'
+    },{
+    	'url':'images/1 (12).jpg',
+    	'price':'12,800'
 }]
 
 //attributes function
@@ -49,6 +50,7 @@ function setAttribute(el,attr){
 const phoneDiv =document.getElementById('phone-row');
 const table = document.querySelector('.table');
 const cartDiv = document.querySelector('.cart');
+const purchaseButton = document.querySelector('.purchase-btn');
 
 //
 images.forEach(image=>{
@@ -81,6 +83,7 @@ images.forEach(image=>{
     setAttribute(quantity,{"type":"number","class":"quantity","value":"1","min":"1"});
     setAttribute(removeBtn,{"class":"btn btn-danger"});
     setAttribute(data2,{"class":"price","value":image.price});
+    setAttribute(row,{"class":"row-items"})
 
     //text contents
     price.textContent= "Ksh "+image.price;
@@ -100,6 +103,7 @@ images.forEach(image=>{
     //eventlistener
     shopbtn.addEventListener('click',(e)=>{
     	e.preventDefault();
+        purchaseButton.setAttribute("data-target","#purchasemodal")
     	table.appendChild(row);
     	row.append(data1,data2,data3,data4);
     	//data1.appendChild(img);
@@ -113,6 +117,7 @@ images.forEach(image=>{
     removeBtn.addEventListener('click',(e)=>{
     	e.preventDefault();
     	table.removeChild(row);
+        dataToggle();
         getTotals();
     })
     quantity.addEventListener('change',getTotals);
@@ -131,7 +136,7 @@ function getTotals(){
         var eachPrice = parseFloat(prices[i].innerHTML.replace(',',''))
         priceArr.push(eachPrice)
     }
-    
+
     for(var i =0; i< itemQuantity.length;i++){
         var eachQuantity = parseFloat(itemQuantity[i].value);
         quantityArr.push(eachQuantity);
@@ -143,4 +148,12 @@ function getTotals(){
 
     totalsHolder.innerHTML = "Ksh "+totals;
     return totalsHolder.innerHTML;
+}
+
+function dataToggle(){
+    const rowItems = document.getElementsByClassName('row-items');
+    let rowItemLength = rowItems.length
+    if(rowItemLength === 0){
+        purchaseButton.removeAttribute('data-target')
+    }
 }
